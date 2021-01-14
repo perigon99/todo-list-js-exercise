a4f79a4f67e989f08f509653f9d64473ee77a14c
Sebastien <perigon@hotmail.com>
2021-01-14 23:11:30 +0000

    assignement completed

--- index.js
+++ index.js
@@ -18,17 +18,6 @@ function newTask(title, description) {
 
 
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-
-
-}
-
 // DRIVER CODE BELOW
 
 
